8468837875a8c77850fe3eab43d22de689838b7a
Medhavi Sahgal <112403556+medhavisahgal@users.noreply.github.com>
2023-05-07 15:31:36 +0530

    Create Bun lover.cpp

---
+++ Bun lover.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        long long int n;
+        cin>>n;
+        long long int ans=((n+1)*(n+1))+1;
+        cout<<ans<<endl;
+    }
+return 0;}
