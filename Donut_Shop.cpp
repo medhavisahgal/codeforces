f938dbee337d64bb4c0a237087c6ed642d567f4b
Medhavi Sahgal <112403556+medhavisahgal@users.noreply.github.com>
2023-07-15 21:12:40 +0530

    Create Donut_Shop.cpp

---
+++ Donut_Shop.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long int a,b,c;
+        cin>>a>>b>>c;
+        long long int pro=b*a;
+        if(a<c){
+            cout<<1<<" ";
+        }
+        else{
+            cout<<-1<<" ";
+        }
+        if(pro>c){
+            cout<<b<<" ";
+        }
+        else{
+            cout<<-1<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
